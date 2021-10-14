610b5008bc602c38d752567f2596e1e8dafea8fc
iharshal0808 <69026742+iharshal0808@users.noreply.github.com>
2021-10-14 12:22:56 +0530

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -19,6 +19,79 @@ class stack{
 void stack::push(int num){
 	if(tos==size-1)	
 	{
+		cout<<"\nStack is full";
+		return;
+	}
+	else{
+		tos++;
+		x[tos]=num;
+		cout<<"\nElement inserted: "<<x[tos];
+	}
+}
+void stack::traverse(){
+	int i;
+	if(tos==-1)
+	{
+		cout<<"\n\nStack is empty\n";
+		return;
+	}
+	else{
+		cout<<"\n\n";
+		for(i=0;i<=tos;i++)
+		{
+			cout<<"->"<<x[i];
+		}
+		cout<<"\n";
+	}
+}
+void stack::pop(){
+	if(tos==-1)
+	{
+		cout<<"\nStack is empty\n";
+		return;
+	}
+	else{
+		num=x[tos];
+		tos--;
+		cout<<"\nDeleted element is:"<<num;
+	}
+}
+int main(){
+	int num;
+	stack obj;
+	obj.push(10);
+	obj.push(20);
+	obj.push(30);
+	obj.traverse();
+	obj.pop();
+	obj.pop();
+	obj.traverse();
+	obj.pop();
+	obj.traverse();
+}
+
+
+/* Program which is Menu driven & asking to you for multiple insertion & deletion in stack */
+
+using namespace std;
+#include<iostream>
+#include<process.h>
+#define size 5
+class stack{
+	private:
+		int num,tos;
+		int x[size];
+	public:
+		stack(){
+			tos=-1;
+		}
+		void push(int num);	
+		void traverse();
+		void pop();
+};
+void stack::push(int num){
+	if(tos==size-1)	
+	{
 		cout<<"Stack is full";
 		return;
 	}
